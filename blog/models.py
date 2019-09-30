0964624dc44e4e796aa9b2002eff100800314875
Tola Tess <x17131553@student.ncirl.ie>
2019-09-30 17:55:01 +0100

    Database models
    created database using django and added some information to the database and deleted dummy data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# What we want to save in the database, Django already have a user model included
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted, there post will also be deleted
+
+    def __str__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'TolaO',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'September 30, 2019',
-    },
-    {
-        'author': 'DorcasO',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 31, 2019',
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     #have access to the content in the home templete
     return render(request, 'blog/home.html', context)
