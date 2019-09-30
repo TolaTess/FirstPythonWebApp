9f5415013a1966b8610dd01c2ae3bdbbd9d4c0b1
Tola Tess <x17131553@student.ncirl.ie>
2019-09-30 15:49:15 +0100

    Dummy Data
    using dummy data to generate posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'TolaO',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'September 30, 2019',
+    },
+    {
+        'author': 'DorcasO',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 31, 2019',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    #have access to the content in the home templete
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
